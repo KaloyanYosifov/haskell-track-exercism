d04eec0b798fceccc2ad12a91ed01d8bf25c41c3
KaloyanYosifov <kaloqn665@gmail.com>
2022-05-30 21:19:40 +0200

    Integrate robot solution

--- robot-simulator/src/Robot.hs
+++ robot-simulator/src/Robot.hs
@@ -10,18 +10,39 @@ data Bearing = North
              | East
              | South
              | West
-             deriving (Eq, Show)
+             deriving (Enum, Eq, Show)
 
-data Robot = Dummy
+data Robot = Robot {
+                  x         :: Integer
+                , y         :: Integer
+                , direction :: Bearing
+             }
+
+moveBasedOnBearing :: Robot -> Robot
+moveBasedOnBearing robot@(Robot x' y' dir)
+  | dir == North = Robot x' (y' + 1) dir
+  | dir == East = Robot (x' + 1) y' dir
+  | dir == West = Robot (x' - 1) y' dir
+  | dir == South = Robot x' (y' - 1) dir
+  | otherwise = robot
 
 bearing :: Robot -> Bearing
-bearing robot = error "You need to implement this function."
+bearing = direction
 
 coordinates :: Robot -> (Integer, Integer)
-coordinates robot = error "You need to implement this function."
+coordinates robot = (x robot, y robot)
 
 mkRobot :: Bearing -> (Integer, Integer) -> Robot
-mkRobot direction coordinates = error "You need to implement this function."
+mkRobot dir coord = Robot {x = fst coord, y = snd coord, direction = dir}
+
+turn :: (Int -> Int) -> Bearing -> Bearing
+turn f dir = toEnum (f (fromEnum dir) `mod` 4)
 
 move :: Robot -> String -> Robot
-move robot instructions = error "You need to implement this function."
+move robot [] = robot
+move robot@(Robot x' y' dir) [d]
+  | d == 'A' = moveBasedOnBearing robot
+  | d == 'R' = Robot x' y' $ turn (+1) dir
+  | d == 'L' = Robot x' y' $ turn (1 `subtract`) dir
+  | otherwise = robot
+move robot (d:instructions) = move (move robot [d]) instructions
