884ca975da0e94de1447d128adcec27e321768ac
KaloyanYosifov <kaloqn665@gmail.com>
2022-06-19 12:08:53 +0200

    Implement solution

--- pascals-triangle/src/Triangle.hs
+++ pascals-triangle/src/Triangle.hs
@@ -1,4 +1,13 @@
 module Triangle (rows) where
 
+rows' :: Int -> [[Integer]]
+rows' x
+    | x == 0 = []
+    | x == 1 = [[1]]
+    | otherwise = (1:evaluatedRow):previousRows
+    where previousRows = rows' (x - 1)
+          previousRow = head previousRows
+          evaluatedRow = zipWith (+) (init previousRow) (tail previousRow) ++ [1]
+
 rows :: Int -> [[Integer]]
-rows x = error "You need to implement this function."
+rows = reverse . rows'
